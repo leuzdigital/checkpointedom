6badacb4a8ba94c07cf133155ca0cfa02261b151
leuzdigital <130456240+leuzdigital@users.noreply.github.com>
2023-07-23 09:08:55 +0000

    Update script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+function changeColor(elementHTML) {
+    
+  elementHTML.classList.toggle("coeur-like")
+  }
+  function removeList(elementHTML ){
+    elementHTML.parentElement.parentElement.remove()
+  }
